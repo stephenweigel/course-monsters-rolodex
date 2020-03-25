355c1ea621f5a16df855123b8923d1786a08458d
Stephen Weigel <stephen@stephenweigel.com>
2020-03-25 15:35:00 -0500

    Updates

---
+++ precache-manifest.ebb6e0d8ecd22a1165f9698c8c8ed8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649ebed0fc71484471e7788693ebe4f6",
+    "url": "/course-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d9eb62a869f5653ce6f9",
+    "url": "/course-monsters-rolodex/static/css/main.48f5f775.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/course-monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/course-monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9eb62a869f5653ce6f9",
+    "url": "/course-monsters-rolodex/static/js/main.16c1065a.chunk.js"
+  },
+  {
+    "revision": "1852bf2dc8af24099605",
+    "url": "/course-monsters-rolodex/static/js/runtime-main.d3133984.js"
+  }
+]);
